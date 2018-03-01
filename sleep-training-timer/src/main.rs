0f0d5861978fd80c7defb669bd4d45fceb4d36f9
Jackson Egan <jackson@sneagan.com>
2018-03-01 00:33:53 -0800

    Working and fancy CLI

--- sleep-training-timer/src/main.rs
+++ sleep-training-timer/src/main.rs
@@ -1,23 +1,37 @@
-extern crate chrono;
+extern crate time;
 #[macro_use]
 extern crate futures;
 extern crate tokio;
 extern crate tokio_timer;
+extern crate clap;
+extern crate indicatif;
+extern crate console;
 
+use indicatif::{ProgressBar, ProgressStyle};
+use clap::{Arg, App, SubCommand};
 use tokio_timer::*;
 use tokio::executor::current_thread;
-use std::time::*;
+use time::{SteadyTime, Duration};
 use futures::{Future, Stream, Async};
 
 
 fn main() {
-    let seconds_passed = timer_sequence_for_day(1);
-    println!("{:?}", seconds_passed);
+    let matches = App::new("Infant Sleep Training Timer")
+        .version("0.1.0")
+        .author("Jackson Egan")
+        .about("I am not a doctor. Use for sleep training at your own risk.")
+        .arg(Arg::with_name("night")
+             .short("n")
+             .long("night")
+             .value_name("NIGHT")
+             .help("Tonight is night X of sleep training. [i.e. 1]")
+             .required(true))
+        .get_matches();
+    let night = matches.value_of("night").unwrap_or("1");
+    let seconds_passed = timer_sequence_for_day(night.parse::<i32>().unwrap());
 
     pub fn timer_sequence_for_day(day_number: i32) -> i32 {
-        let mut seconds_passed_this_loop = 0;
-        let mut seconds_passed_total = 0;
-        let durations = match day_number {
+        let durations: Vec<u64> = match day_number {
             1 => vec![3, 5, 10, 10, 10, 10],
             2 => vec![5, 10, 12, 12, 12, 12],
             3 => vec![10, 12, 15, 15, 15, 15],
@@ -28,21 +42,25 @@ fn main() {
             _ => vec![0],
         };
         
-        for duration in durations {
+        for (i, duration) in durations.iter().enumerate() {
+            let duration_fmt: i64 = duration.to_string().parse::<i64>().unwrap();
+            let start_time_plus = SteadyTime::now() + Duration::minutes(duration_fmt) + Duration::seconds(2);
+            println!("Interval {}.", i+1);
+            let bar = ProgressBar::new(duration*60);
+            bar.set_style(ProgressStyle::default_bar()
+                .template("[{eta_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} {msg}"));
             Timer::default()
-                .interval(Duration::new(1, 0))
-                .take(5)
+                .interval(Duration::seconds(1).to_std().unwrap())
+                .take(duration*60)
                 .for_each(|val| {
-                    seconds_passed_this_loop = seconds_passed_this_loop + 1;
-                    seconds_passed_total = seconds_passed_total + 1;
-                    println!("{:?}", seconds_passed_this_loop);
+                    let secs = (start_time_plus - SteadyTime::now()).num_seconds();
+                    bar.inc(1);
                     Ok(val)
                 })
                 .wait();
-            seconds_passed_this_loop = 0;
+                bar.finish();
         }
-
-        seconds_passed_total
+        0
     }
 }
 
