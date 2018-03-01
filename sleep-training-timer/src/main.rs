fa0b8cea2b8fb2c0eecf1200a5dc06c920abcb04
Jackson Egan <jackson@sneagan.com>
2018-02-28 21:37:01 -0800

    WIP logging in loops

--- sleep-training-timer/src/main.rs
+++ sleep-training-timer/src/main.rs
@@ -1,16 +1,48 @@
 extern crate chrono;
-extern crate tokio_core;
-extern crate tokio_timer;
+#[macro_use]
 extern crate futures;
+extern crate tokio;
+extern crate tokio_timer;
 
-use std::{thread, time};
+use tokio_timer::*;
+use tokio::executor::current_thread;
+use std::time::*;
+use futures::{Future, Stream, Async};
 
-mod sleep_timer;
 
 fn main() {
-    let mut timer_manager = sleep_timer::TimerManager::new(1);
-    let timer = timer_manager.start_timer_sequence();
-    println!("{:?}", timer.time_passed());
-    thread::sleep(time::Duration::from_millis(5000));
-    println!("{:?}", timer.time_passed());
+    let seconds_passed = timer_sequence_for_day(1);
+    println!("{:?}", seconds_passed);
+
+    pub fn timer_sequence_for_day(day_number: i32) -> i32 {
+        let mut seconds_passed_this_loop = 0;
+        let mut seconds_passed_total = 0;
+        let durations = match day_number {
+            1 => vec![3, 5, 10, 10, 10, 10],
+            2 => vec![5, 10, 12, 12, 12, 12],
+            3 => vec![10, 12, 15, 15, 15, 15],
+            4 => vec![12, 15, 17, 17, 17, 17],
+            5 => vec![15, 17, 20, 20, 20, 20],
+            6 => vec![17, 20, 25, 25, 25, 25],
+            7 => vec![20, 25, 30, 30, 30, 30],
+            _ => vec![0],
+        };
+        
+        for duration in durations {
+            Timer::default()
+                .interval(Duration::new(1, 0))
+                .take(5)
+                .for_each(|val| {
+                    seconds_passed_this_loop = seconds_passed_this_loop + 1;
+                    seconds_passed_total = seconds_passed_total + 1;
+                    println!("{:?}", seconds_passed_this_loop);
+                    Ok(val)
+                })
+                .wait();
+            seconds_passed_this_loop = 0;
+        }
+
+        seconds_passed_total
+    }
 }
+
